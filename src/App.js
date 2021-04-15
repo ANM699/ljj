4b784ec70f90273471f8cdbb71d253930363772e
ANM699 <kevin_kuo@126.com>
2021-04-15 23:53:13 +0800

    实现基本功能

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 // import logo from "./logo.svg";
-import { useRef } from "react";
-import { Button, InputItem } from "antd-mobile";
-
+import { createForm } from "rc-form";
+import {
+  Button,
+  InputItem,
+  List,
+  DatePicker,
+  Stepper,
+  WingBlank,
+  WhiteSpace,
+} from "antd-mobile";
 import { genReport } from "./utils/index";
 // import "./App.css";
 
-function App() {
-  const fileInput = useRef(null);
+const Item = List.Item;
 
+function App({ form }) {
+  //上传模版
+  // const fileInput = useRef(null);
+  // const handleClick = () => {
+  //   const file = fileInput.current.inputRef.inputRef.files[0];
+  //   genReport(file);
+  // };
   const handleClick = () => {
-    console.log(fileInput.current);
-    console.log(fileInput.current.inputRef.inputRef.files[0]);
-    const file = fileInput.current.inputRef.inputRef.files[0];
-    genReport(file);
+    console.log(form.getFieldsValue());
+    const formData = form.getFieldsValue();
+    const c = {
+      position: formData.position,
+      section: [formData.mValue1, formData.mValue2, formData.mValue3],
+    };
+    const data = { ...formData, c };
+    genReport(data);
   };
-
+  const { getFieldProps } = form;
   return (
-    <div>
-      <InputItem type="file" ref={fileInput}></InputItem>
-      <Button onClick={handleClick}>导出</Button>
-    </div>
+    <WingBlank>
+      <WhiteSpace />
+      <List style={{ backgroundColor: "red" }}>
+        <InputItem {...getFieldProps("projectName")}>项目名称</InputItem>
+        <InputItem labelNumber={7} {...getFieldProps("equipmentName")}>
+          检测设备及名称
+        </InputItem>
+        <DatePicker
+          mode="date"
+          title="Select Date"
+          {...getFieldProps("detectDate")}
+        >
+          <Item>检测时间</Item>
+        </DatePicker>
+        <InputItem type="number" {...getFieldProps("reportNo")}>
+          报告编号
+        </InputItem>
+        <InputItem {...getFieldProps("position")}>柱位置</InputItem>
+        <InputItem type="number" {...getFieldProps("mValue1")}>
+          实测值1
+        </InputItem>
+        <InputItem type="number" {...getFieldProps("mValue2")}>
+          实测值2
+        </InputItem>
+        <InputItem type="number" {...getFieldProps("mValue3")}>
+          实测值3
+        </InputItem>
+      </List>
+      <WhiteSpace />
+      <Button type="primary" onClick={handleClick}>
+        导出
+      </Button>
+    </WingBlank>
   );
 }
 
-export default App;
+export default createForm()(App);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,42 +1,50 @@
 import createReport from "docx-templates";
 import JSZipUtils from "jszip-utils";
 
-export async function genReport(file) {
-  const template = await readFileIntoArrayBuffer(file);
-  const report = createReport({
-    template,
-    data: {
-      name: "John",
-      // surname: "Appleseed",
-    },
-    // cmdDelimiter: ["{", "}"],
-  });
-  // Save report
-  saveDataToFile(
-    report,
-    "report.docx",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  );
-  // JSZipUtils.getBinaryContent(
-  //   "./templates/myTemplate.docx",
-  //   async (error, template) => {
-  //     if (error) throw error;
-  //     const report = createReport({
-  //       template,
-  //       data: {
-  //         name: "John",
-  //         surname: "Appleseed",
-  //       },
-  //       cmdDelimiter: ["{", "}"],
-  //     });
-  //     // Save report
-  //     saveDataToFile(
-  //       report,
-  //       "report.docx",
-  //       "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  //     );
-  //   }
+export async function genReport(data) {
+  //上传模版
+  // const template = await readFileIntoArrayBuffer(file);
+  // const report = await createReport({
+  //   template,
+  //   data: {
+  //     name: "John",
+  //     surname: "Appleseed",
+  //   },
+  //   cmdDelimiter: ["{", "}"],
+  // });
+  // saveDataToFile(
+  //   report,
+  //   "report.docx",
+  //   "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
   // );
+
+  JSZipUtils.getBinaryContent(
+    "./templates/F-05.docx",
+    async (error, template) => {
+      if (error) throw error;
+      const report = await createReport({
+        template,
+        // data: {
+        //   projectName: "测试项目",
+        //   equipmentName: "设备",
+        //   detectDate: "2021-4-15",
+        //   reportNo: "292831930",
+        //   c: {
+        //     position: "北",
+        //     section: [23, 21, 36],
+        //   },
+        // },
+        data,
+        cmdDelimiter: ["{", "}"],
+      });
+      // Save report
+      saveDataToFile(
+        report,
+        "report.docx",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      );
+    }
+  );
 }
 
 // ==============================================
