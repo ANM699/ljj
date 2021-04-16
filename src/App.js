412908cae22cfc083c94c1ce6c551f6f2de63b52
@guo.yunxiang <guo.yunxinag@bestv.com.cn>
2021-04-16 13:37:11 +0800

    完成demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from "./logo.svg";
-import { createForm } from "rc-form";
+import { format } from 'date-fns';
+import { createForm } from 'rc-form';
 import {
   Button,
   InputItem,
   List,
   DatePicker,
-  Stepper,
   WingBlank,
   WhiteSpace,
-} from "antd-mobile";
-import { genReport } from "./utils/index";
+} from 'antd-mobile';
+import { genReport } from './utils/index';
 // import "./App.css";
 
 const Item = List.Item;
@@ -22,42 +22,97 @@ function App({ form }) {
   //   genReport(file);
   // };
   const handleClick = () => {
-    console.log(form.getFieldsValue());
-    const formData = form.getFieldsValue();
-    const c = {
-      position: formData.position,
-      section: [formData.mValue1, formData.mValue2, formData.mValue3],
-    };
-    const data = { ...formData, c };
-    genReport(data);
+    form.validateFields({ force: true }, (error) => {
+      if (!error) {
+        // console.log(form.getFieldsValue());
+        const formData = form.getFieldsValue();
+        formData.detectDate = format(formData.detectDate, 'yyyy-MM-dd');
+        const c = {
+          position: formData.position,
+          section: [formData.mValue1, formData.mValue2, formData.mValue3],
+        };
+        const data = { ...formData, c };
+        console.log(data);
+        genReport(data);
+      } else {
+        // alert('验证失败');
+      }
+    });
   };
-  const { getFieldProps } = form;
+  const { getFieldProps, getFieldError } = form;
   return (
     <WingBlank>
       <WhiteSpace />
-      <List style={{ backgroundColor: "red" }}>
-        <InputItem {...getFieldProps("projectName")}>项目名称</InputItem>
-        <InputItem labelNumber={7} {...getFieldProps("equipmentName")}>
+      <List style={{ backgroundColor: 'red' }}>
+        <InputItem
+          error={!!getFieldError('projectName')}
+          {...getFieldProps('projectName', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
+          项目名称
+        </InputItem>
+        <InputItem
+          error={!!getFieldError('equipmentName')}
+          labelNumber={7}
+          {...getFieldProps('equipmentName', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
           检测设备及名称
         </InputItem>
         <DatePicker
           mode="date"
-          title="Select Date"
-          {...getFieldProps("detectDate")}
+          title="选择日期"
+          {...getFieldProps('detectDate', {
+            initialValue: new Date(),
+            rules: [{ required: true, message: '必填' }],
+          })}
         >
-          <Item>检测时间</Item>
+          <Item error={!!getFieldError('detectDate')}>检测时间</Item>
         </DatePicker>
-        <InputItem type="number" {...getFieldProps("reportNo")}>
+        <InputItem
+          error={!!getFieldError('reportNo')}
+          type="number"
+          {...getFieldProps('reportNo', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
           报告编号
         </InputItem>
-        <InputItem {...getFieldProps("position")}>柱位置</InputItem>
-        <InputItem type="number" {...getFieldProps("mValue1")}>
+        <InputItem
+          error={!!getFieldError('position')}
+          {...getFieldProps('position', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
+          柱位置
+        </InputItem>
+        <InputItem
+          error={!!getFieldError('mValue1')}
+          type="number"
+          {...getFieldProps('mValue1', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
           实测值1
         </InputItem>
-        <InputItem type="number" {...getFieldProps("mValue2")}>
+        <InputItem
+          error={!!getFieldError('mValue2')}
+          type="number"
+          {...getFieldProps('mValue2', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
           实测值2
         </InputItem>
-        <InputItem type="number" {...getFieldProps("mValue3")}>
+        <InputItem
+          error={!!getFieldError('mValue3')}
+          type="number"
+          {...getFieldProps('mValue3', {
+            rules: [{ required: true, message: '必填' }],
+          })}
+        >
           实测值3
         </InputItem>
       </List>
